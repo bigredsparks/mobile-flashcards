0650dfc357bfb0f518ebfdb087fde665c7a12186
Vince Sparks <svsparks@swbell.net>
2018-04-30 21:33:19 -0500

    feat: added Deck and New Deck tabs

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import { TabNavigator } from 'react-navigation'
+import { Constants } from 'expo'
+import { purple } from './utils/colors'
+import Decks from './components/Decks'
+import NewDeck from './components/NewDeck'
+
+function FlashcardStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tabs = TabNavigator({
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: 'Decks'
+    }
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck'
+    }
+  }
+},{
+  navigationOptions: {
+    header: null
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex:1}}>
+        <FlashcardStatusBar backgroundColor={purple} barStyle='light-content'/>
+        <Tabs />
       </View>
-    );
+    )
   }
 }
 
@@ -20,4 +51,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ components/NewDeck.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { View, Text  } from 'react-native'
+
+export default class NewDeck extends Component {
+  render() {
+    return (
+      <Text>New Deck</Text>
+    )
+  }
+}
