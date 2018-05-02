555f37cb8c2d04f4ef102887f00b5b86eceae2c0
Vince Sparks <svsparks@swbell.net>
2018-05-01 19:59:14 -0500

    created 'New Deck' page and added styling

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react'
-import { View, Text  } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet  } from 'react-native'
+import { FontAwesome, Entypo } from '@expo/vector-icons'
+import { white, purple } from '../utils/colors'
 
 export default class NewDeck extends Component {
   render() {
     return (
-      <Text>New Deck</Text>
+      <View>
+        <Text style={styles.question}>
+          What is the title of your new deck?</Text>
+        <TextInput style={styles.input} placeholder={'Deck Title'}/>
+        <TouchableOpacity
+          style={styles.button}>
+          <Text style={styles.btnText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
     )
   }
 }
+
+const styles=StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  question: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginLeft: 60,
+    marginRight: 60,
+    textAlign: 'center',
+  },
+  input: {
+    backgroundColor: white,
+    borderColor: purple,
+    borderWidth: 1,
+    borderRadius: 4,
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+    marginBottom: 20,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 5,
+  },
+  button: {
+    marginLeft: 90,
+    marginRight: 90,
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnText: {
+    color: white,
+    alignItems: 'center',
+  }
+})
