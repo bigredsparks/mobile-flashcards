219b8744f7bf0ec980c4c11797d6cde9eb40eda4
Vince Sparks <svsparks@swbell.net>
2018-05-12 16:03:03 -0500

    added initial shell component for quiz

---
+++ components/Quiz.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet  } from 'react-native'
+import { white, purple } from '../utils/colors'
+
+export default class Quiz extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Quiz
+        </Text>
+      </View>
+    )
+  }
+}
