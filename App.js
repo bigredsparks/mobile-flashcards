6877d1aaceacd2a596a73c78615db889b0c0f80c
Vince Sparks <svsparks@swbell.net>
2018-05-12 12:28:07 -0500

    Added navigation and provider for redux store

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View, StatusBar } from 'react-native'
-import { TabNavigator } from 'react-navigation'
-import { Constants } from 'expo'
-import { purple } from './utils/colors'
+import {StyleSheet, View, StatusBar, Platform} from 'react-native'
+import {TabNavigator, StackNavigator} from 'react-navigation'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import {Constants} from 'expo'
+import {white, purple} from './utils/colors'
 import Decks from './components/Decks'
 import NewDeck from './components/NewDeck'
+import DeckView from './components/DeckView'
 
-function FlashcardStatusBar ({backgroundColor, ...props}) {
+console.disableYellowBox = true
+
+function FlashcardStatusBar({backgroundColor, ...props}) {
   return (
     <View style={{backgroundColor, height: Constants.statusBarHeight}}>
       <StatusBar translucent backgroundColor={backgroundColor} {...props} />
@@ -27,19 +33,37 @@ const Tabs = TabNavigator({
       tabBarLabel: 'New Deck'
     }
   }
-},{
+}, {
   navigationOptions: {
     header: null
   }
 })
 
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  DeckView: {
+    screen: DeckView,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      }
+    }
+  }
+})
+
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{flex:1}}>
-        <FlashcardStatusBar backgroundColor={purple} barStyle='light-content'/>
-        <Tabs />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+          <FlashcardStatusBar backgroundColor={purple} barStyle='light-content'/>
+          <MainNavigator/>
+        </View>
+      </Provider>
     )
   }
 }
