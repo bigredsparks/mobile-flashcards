aac59ea33da496063b7377726f253259cad5ab73
Vince Sparks <svsparks@swbell.net>
2018-05-12 21:07:39 -0500

    implemented api functions to read and write from Async storage

--- utils/api.js
+++ utils/api.js
@@ -28,18 +28,39 @@ const FLASHCARD_STORAGE_KEY = 'mobile-flashcard'
 //   }
 // }
 
-export function getDecks () {
+export function deleteAllDecks() {
+  return AsyncStorage.removeItem(FLASHCARD_STORAGE_KEY)
+}
 
+export function getDecks () {
+  return AsyncStorage.getItem(FLASHCARD_STORAGE_KEY)
+    .then((results) => (JSON.parse(results)))
 }
 
 export function getDeck (id) {
-
+  return AsyncStorage.getItem(FLASHCARD_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      data[id]
+    })
 }
 
 export function saveDeckTitle (title) {
-
+  return AsyncStorage.mergeItem(
+    FLASHCARD_STORAGE_KEY,
+    JSON.stringify({
+      [title] : {
+        title: title,
+        questions: []
+      },
+  }))
 }
 
 export function addCardToDeck (card, title) {
-
+  return AsyncStorage.getItem(FLASHCARD_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      data[title].questions.push(card)
+      AsyncStorage.setItem(FLASHCARD_STORAGE_KEY, JSON.stringify(data))
+    })
 }
